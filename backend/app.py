708a5e382d98dce4936e59a28f2d782930168f68
omarWRLD <63510451+OmarTl2005@users.noreply.github.com>
2024-03-27 23:50:55 +0000

    done

--- backend/app.py
+++ backend/app.py
@@ -27,7 +27,8 @@ login_manager.login_view = 'login'
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
+    user = User.query.get(int(user_id))
+    return user
 
 # managin models
 class User(UserMixin, db.Model):
